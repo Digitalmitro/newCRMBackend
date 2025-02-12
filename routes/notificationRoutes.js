6376345796c172c66d72f7414ab035f3c9a0b28f
digitalmitro <digitalmitrous@gmail.com>
2025-02-12 18:18:14 +0530

    notify

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,10 +1,10 @@
 // routes/notificationRoutes.js
 const express = require("express");
 const { sendNotification,getNotification } = require("../controllers/notificationController");
-
+const {authMiddleware} =require("../middlewares/authMiddleware")
 const router = express.Router();
 
-router.post("/send-notification", sendNotification);
-router.get("/get-notifications",getNotification)
+router.post("/send-notification",  sendNotification);
+router.get("/get-notifications", authMiddleware,getNotification)
 
 module.exports = router;
