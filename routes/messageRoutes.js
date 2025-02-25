8b8c36aacb1f15626880fce21eb06d20d1d6a253
soumendigitalmitro <soumen.digitalmitro@gmail.com>
2025-02-25 17:17:45 +0530

    add unread

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,14 +1,14 @@
 // routes/messageRoutes.js
 const express = require("express");
 const {authMiddleware} = require("../middlewares/authMiddleware")
-const { sendMessage, getMessages, getRecentChatUsers,getAllUser } = require("../controllers/messageController");
+const { sendMessage, getMessages, getRecentChatUsers,getAllUser,readMessage } = require("../controllers/messageController");
 
 const router = express.Router();
 
 router.post("/send-message", sendMessage);
 router.get("/messages/:sender/:receiver", getMessages);
-router.get("/recentChats",authMiddleware,getAllUser )
-router.post("/messages/mark-as-read")
+router.get("/recentChats",authMiddleware,getAllUser);
+router.post("/messages/mark-as-read",authMiddleware,readMessage);
 
 
 module.exports = router;
