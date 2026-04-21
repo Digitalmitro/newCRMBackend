172848e7176a3d28c2e9b20ceedefae35b0ed30b
debasish <assens.1234@gmail.com>
2026-04-22 02:22:47 +0530

    Check router

--- routes/profilePictureRoutes.js
+++ routes/profilePictureRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middlewares/authMiddleware');
-const upload = require('../utils/fileUpload');
+const { authMiddleware }= require('../middlewares/authMiddleware');
+const { upload }= require('../utils/fileUpload');
 const {
   uploadProfilePicture,
   getProfilePicture,
