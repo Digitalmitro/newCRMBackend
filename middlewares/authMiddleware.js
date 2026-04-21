a102d611b6baac7fe0c473fa0307b0dd78b78623
debasish <assens.1234@gmail.com>
2026-04-22 02:08:25 +0530

    Check router

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,8 +1,19 @@
-// backend/middleware/authMiddleware.js
-const jwt = require('jsonwebtoken');
-exports.authMiddleware = (req, res, next) => {
+// // backend/middleware/authMiddleware.js
+// const jwt = require('jsonwebtoken');
+// exports.authMiddleware = (req, res, next) => {
+//   const token = req.header('Authorization')?.split(" ")[1];
+//   if (!token) return res.status(401).json({ message: "Unauthorized" });
+//   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+//     if (err) return res.status(403).json({ message: "Forbidden" });
+//     req.user = user;
+//     next();
+//   });
+// };
+
+module.exports = (req, res, next) => {
   const token = req.header('Authorization')?.split(" ")[1];
   if (!token) return res.status(401).json({ message: "Unauthorized" });
+
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.status(403).json({ message: "Forbidden" });
     req.user = user;
