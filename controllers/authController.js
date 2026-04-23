f55e49199c5d0882b9a144b1bc6985cee2c26a7c
debasish <assens.1234@gmail.com>
2026-04-23 11:40:10 +0530

    admin type added

--- controllers/authController.js
+++ controllers/authController.js
@@ -87,7 +87,7 @@ exports.getUserName = async (req, res) => {
 // Admin Signup
 exports.adminSignup = async (req, res) => {
   try {
-    const { name, email, phone, password } = req.body;
+    const { name, email, phone, password, type } = req.body;
 
     // Check if admin already exists
     const existingAdmin = await RegisteradminModal.findOne({ email });
@@ -105,6 +105,7 @@ exports.adminSignup = async (req, res) => {
       email,
       phone,
       password: hashedPassword,
+      type:type
     });
 
     await newAdmin.save();
